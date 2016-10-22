3ca16827a103270ebc36f98b23986763afcd31af
Guido van Rossum <guido@python.org>
2016-10-22 07:18:47 -0700

    Also copy test_typing.py to mypy repo.

--- update-mypy.sh
+++ update-mypy.sh
@@ -9,6 +9,11 @@ HERE=$PWD
 cd ~/src/mypy
 git co master
 git pull
+
 cp $HERE/src/typing.py lib-typing/3.2/typing.py
+cp $HERE/src/test_typing.py lib-typing/3.2/test_typing.py
+
 cp $HERE/python2/typing.py lib-typing/2.7/typing.py
+cp $HERE/python2/test_typing.py lib-typing/2.7/test_typing.py
+
 git ci lib-typing -m "$@"
