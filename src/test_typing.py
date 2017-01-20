aea09810312857df65f79aa63956237576ae3e5a
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-01-20 22:56:48 +0100

    Allow defining methods in NamedTuple class syntax

--- src/test_typing.py
+++ src/test_typing.py
@@ -1413,6 +1413,16 @@ class CoolEmployee(NamedTuple):
 class CoolEmployeeWithDefault(NamedTuple):
     name: str
     cool: int = 0
+
+class XMeth(NamedTuple):
+    x: int
+    def double(self):
+        return 2 * self.x
+
+class XMethBad(NamedTuple):
+    x: int
+    def _fields(self):
+        return 'no chance for this'
 """
 
 if PY36:
@@ -2034,6 +2044,13 @@ class NamedTupleTests(BaseTestCase):
         self.assertEqual(CoolEmployeeWithDefault._fields, ('name', 'cool'))
         self.assertEqual(CoolEmployeeWithDefault._field_types, dict(name=str, cool=int))
         self.assertEqual(CoolEmployeeWithDefault._field_defaults, dict(cool=0))
+
+    @skipUnless(PY36, 'Python 3.6 required')
+    def test_annotation_usage_with_methods(self):
+        self.assertEquals(XMeth(1).double(), 2)
+        self.assertEquals(XMeth(42).x, XMeth(42)[0])
+        self.assertEquals(XMethBad(1)._fields, ('x',))
+        self.assertEquals(XMethBad(1).__annotations__, {'x': int})
 
         with self.assertRaises(TypeError):
             exec("""
