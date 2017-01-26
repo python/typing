da58fa754523a3cdfaaac5d3d9f6cbc4a95fa7e0
Ivan Levkivskyi <levkivslyi@gmail.com>
2017-01-26 20:53:27 +0100

    Fix hash of GenericMeta classes

--- python2/test_typing.py
+++ python2/test_typing.py
@@ -707,6 +707,42 @@ class GenericTests(BaseTestCase):
         self.assertEqual(Union[T, int][GenericMeta], Union[GenericMeta, int])
         self.assertEqual(Callable[..., GenericMeta].__args__, (Ellipsis, GenericMeta))
 
+    def test_generic_hashes(self):
+        import mod
+        class A(Generic[T]):
+            pass
+
+        class B(Generic[T]):
+            class A(Generic[T]):
+                pass
+
+        self.assertEqual(A, A)
+        self.assertEqual(mod.A[str], mod.A[str])
+        self.assertEqual(B.A, B.A)
+        self.assertEqual(mod.B.A[B.A[str]], mod.B.A[B.A[str]])
+
+        self.assertNotEqual(A, B.A)
+        self.assertNotEqual(A, mod.A)
+        self.assertNotEqual(A, mod.B.A)
+        self.assertNotEqual(B.A, mod.A)
+        self.assertNotEqual(B.A, mod.B.A)
+
+        self.assertNotEqual(A[str], B.A[str])
+        self.assertNotEqual(A[List[Any]], B.A[List[Any]])
+        self.assertNotEqual(A[str], mod.A[str])
+        self.assertNotEqual(A[str], mod.B.A[str])
+        self.assertNotEqual(B.A[int], mod.A[int])
+        self.assertNotEqual(B.A[List[Any]], mod.B.A[List[Any]])
+
+        self.assertNotEqual(Tuple[A[str]], Tuple[B.A[str]])
+        self.assertNotEqual(Tuple[A[List[Any]]], Tuple[B.A[List[Any]]])
+        self.assertNotEqual(Union[str, A[str]], Union[str, mod.A[str]])
+        self.assertNotEqual(Union[A[str], A[str]], Union[A[str], mod.A[str]])
+        self.assertNotEqual(typing.FrozenSet[A[str]], typing.FrozenSet[mod.B.A[str]])
+
+        self.assertTrue(repr(Tuple[A[str]]).endswith('typing.A[str]]'))
+        self.assertTrue(repr(Tuple[mod.A[str]]).endswith('mod.A[str]]'))
+
     def test_extended_generic_rules_eq(self):
         T = TypeVar('T')
         U = TypeVar('U')
