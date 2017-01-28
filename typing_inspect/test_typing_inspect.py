d8270bbe22ce18034f33721a558b62d8f1fd5a57
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-01-28 23:33:03 +0100

    Add docstrings plus few tests

--- typing_inspect/test_typing_inspect.py
+++ typing_inspect/test_typing_inspect.py
@@ -1,5 +1,9 @@
 from typing_inspect import is_generic_type
 from unittest import TestCase, main
+from typing import (
+    Union, ClassVar, Callable, Optional, TypeVar, Sequence, Mapping,
+    MutableMapping, Iterable, Generic, List
+)
 
 
 class IsUtilityTestCase(TestCase):
@@ -10,8 +14,11 @@ class IsUtilityTestCase(TestCase):
             self.assertFalse(fun(s))
 
     def test_generic(self):
-        samples = []
-        nonsamples = []
+        T = TypeVar('T')
+        samples = [Generic, Generic[T], Iterable[int], Mapping,
+                   MutableMapping[T, List[int]], Sequence[Union[str, bytes]]]
+        nonsamples = [int, Union[int, str], Union[int, T], ClassVar[List[int]],
+                      Callable[..., T], ClassVar, Optional, bytes, list]
         self.sample_test(is_generic_type, samples, nonsamples)
 
 
--- typing_inspect/typing_inspect.py
+++ typing_inspect/typing_inspect.py
@@ -30,10 +30,39 @@ def _eval_args(args):
 
 
 def is_generic_type(tp):
-    return isinstance(tp, GenericMeta)
+    """Test if the given type is a generic type. This includes Generic itself, but
+    excludes special typing constructs such as Union, Tuple, Callable, ClassVar.
+    Examples::
+
+        is_generic_type(int) == False
+        is_generic_type(Union[int, str]) == False
+        is_generic_type(Union[int, T]) == False
+        is_generic_type(ClassVar[List[int]]) == False
+        is_generic_type(Callable[..., T]) == False
+
+        is_generic_type(Generic) == True
+        is_generic_type(Generic[T]) == True
+        is_generic_type(Iterable[int]) == True
+        is_generic_type(Mapping) == True
+        is_generic_type(MutableMapping[T, List[int]]) == True
+        is_generic_type(Sequence[Union[str, bytes]]) == True
+    """
+
+    return type(tp) is GenericMeta
 
 
 def get_origin(tp):
+    """Get the unsubscripted version of a type. Supports generic types, Union,
+    Callable, and Tuple. Returns None for unsupported types. Examples::
+
+        get_origin(int) == None
+        get_origin(ClassVar[int]) == None
+        get_origin(Generic) == Generic
+        get_origin(Generic[T]) == Generic
+        get_origin(Union[T, int]) == Union
+        get_origin(List[Tuple[T, T]][int]) == List
+    """
+
     if is_generic_type(tp):
         return _gorg(tp)
     if is_union_type(tp):
@@ -42,32 +71,99 @@ def get_origin(tp):
 
 
 def is_callable_type(tp):
+    """Test if the type is a generic callable type. Examples::
+
+        is_callable_type(int) == False
+        is_callable_type(type) == False
+        is_callable_type(Callable) == True
+        is_callable_type(Callable[..., int]) == True
+        is_callable_type(Callable[[int, int], Iterable[str]]) == True
+    """
+
     return get_origin(tp) is Callable
 
 
 def is_tuple_type(tp):
+    """Test if the type is a generic tuple type. Examples::
+
+        is_tuple_type(int) == False
+        is_tuple_type(tuple) == False
+        is_tuple_type(Tuple) == True
+        is_tuple_type(Tuple[str, int]) == True
+    """
+
     return get_origin(tp) is Tuple
 
 
 def is_union_type(tp):
+    """Test if the type is a union type. Examples::
+
+        is_union_type(int) == False
+        is_union_type(Union) == True
+        is_union_type(Union[int, int]) == False
+        is_union_type(Union[T, int]) == True
+    """
+
     return type(tp) is _Union
 
 
 def is_typevar(tp):
+    """Test if the type represents a type variable. Examples::
+
+        is_typevar(int) == False
+        is_typevar(T) == True
+        is_typevar(Union[T, int]) == False
+    """
+
     return type(tp) is TypeVar
 
 
 def is_classvar(tp):
+    """Test if the type represents a class variable. Examples::
+
+        is_classvar(int) == False
+        is_classvar(ClassVar) == True
+        is_classvar(ClassVar[int]) == True
+        is_classvar(ClassVar[List[T]]) == True
+    """
+
     return type(tp) is _ClassVar
 
 
 def get_parameters(tp):
-    if is_generic_type(tp) or is_union_type(tp):
+    """Return type parameters of a parameterizable type as a tuple
+    in lexicographic order. Parameterizable types are generic types,
+    unions, tuple types and callable types. Examples::
+
+        get_parameters(int) == ()
+        get_parameters(Generic) == ()
+        get_parameters(Union) == ()
+        get_parameters(List[int]) == ()
+
+        get_parameters(Generic[T]) == (T,)
+        get_parameters(Tuple[List[T], List[S]]) == (T, S)
+        get_parameters(Union[S, Tuple[T, T]][int, U]) == (U,)
+        get_parameters(Mapping[T, Tuple[S, T]]) == (T, S)
+    """
+
+    if (
+        is_generic_type(tp) or is_union_type(tp) or
+        is_callable_type(tp) or is_tuple_type(tp)
+    ):
         return tp.__parameters__
     return ()
 
 
 def get_last_args(tp):
+    """Get last arguments of (multiply) subscripted type. Examples::
+
+        get_last_args(int) == ()
+        get_last_args(Union) == ()
+        get_last_args(ClassVar[int]) == (int,)
+        get_last_args(Union[T, int]) == (int,)
+        get_last_args(Iterable[Tuple[T, S]][int, T]) == (int, T)
+    """
+
     if is_classvar(tp):
         return (tp.__type__,)
     if is_generic_type(tp) or is_union_type(tp):
@@ -76,6 +172,20 @@ def get_last_args(tp):
 
 
 def get_args(tp, evaluate=False):
+    """Get type arguments with all substitutions performed. For unions,
+    basic simplifications used by Union constructor are performed.
+    If `evaluate` is False (default), report result as nested tuple, this matches
+    the internal representation of types. If `evaluate` is True, then all
+    type parameters are applied (this could be expensive). Examples::
+
+        get_args(Union[int, Tuple[T, int]][str]) == (int, (Tuple, str, int))
+        get_args(Union[int, Union[T, int], str][int]) == (int, str)
+        get_args(int) == ()
+
+        get_args(Dict[int, Tuple[T, T]][Optional[int]], evaluate=True) == \
+                 (int, Tuple[Optional[int], Optional[int]])
+    """
+
     if is_classvar(tp):
         return (tp.__type__,)
     if is_generic_type(tp) or is_union_type(tp):
