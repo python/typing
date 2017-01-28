a0fbbe80644b7928972bc035f9b96e10bbe34316
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-01-28 22:28:12 +0100

    Fix some lint

--- typing_inspect/test_typing_inspect.py
+++ typing_inspect/test_typing_inspect.py
@@ -1,16 +1,19 @@
 from typing_inspect import is_generic_type
 from unittest import TestCase, main
 
+
 class IsUtilityTestCase(TestCase):
     def sample_test(self, fun, samples, nonsamples):
         for s in samples:
             self.assertTrue(fun(s))
         for s in nonsamples:
             self.assertFalse(fun(s))
+
     def test_generic(self):
         samples = []
         nonsamples = []
         self.sample_test(is_generic_type, samples, nonsamples)
+
 
 if __name__ == '__main__':
     main()
--- typing_inspect/typing_inspect.py
+++ typing_inspect/typing_inspect.py
@@ -7,11 +7,10 @@ Example usage::
 
 # NOTE: This module must support Python 2.7 in addition to Python 3.x
 
-import sys
 from typing import (
-    Callable, Union, Tuple, TypeVar, Type, Generic,
-    GenericMeta, _gorg, _Union, _ClassVar,
+    Callable, Union, Tuple, TypeVar, GenericMeta, _gorg, _Union, _ClassVar,
 )
+
 
 def _eval_args(args):
     res = []
@@ -29,8 +28,10 @@ def _eval_args(args):
             res.append(type(arg[0]).__getitem__(arg[0], _eval_args(arg[1:])))
     return tuple(res)
 
+
 def is_generic_type(tp):
     return isinstance(tp, GenericMeta)
+
 
 def get_origin(tp):
     if is_generic_type(tp):
@@ -39,25 +40,32 @@ def get_origin(tp):
         return Union
     return None
 
+
 def is_callable_type(tp):
     return get_origin(tp) is Callable
+
 
 def is_tuple_type(tp):
     return get_origin(tp) is Tuple
 
+
 def is_union_type(tp):
     return type(tp) is _Union
+
 
 def is_typevar(tp):
     return type(tp) is TypeVar
 
+
 def is_classvar(tp):
     return type(tp) is _ClassVar
+
 
 def get_parameters(tp):
     if is_generic_type(tp) or is_union_type(tp):
         return tp.__parameters__
     return ()
+
 
 def get_last_args(tp):
     if is_classvar(tp):
@@ -65,6 +73,7 @@ def get_last_args(tp):
     if is_generic_type(tp) or is_union_type(tp):
         return tp.__args__
     return ()
+
 
 def get_args(tp, evaluate=False):
     if is_classvar(tp):
@@ -76,3 +85,4 @@ def get_args(tp, evaluate=False):
                 return tree[1:]
             return _eval_args(tree[1:]) 
     return ()
+
