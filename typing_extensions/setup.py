e496dd38ba737fcd6ac2f5c2660f7d74e788488f
Ivan Levkivskyi <levkivskyi@gmail.com>
2017-09-17 13:57:49 +0200

    Update version in setup.py, fix typo, and update contact e-mail (#473)
    This fixes a typo, updates required `typing` version to the latest one,
    updates the contact e-mail, and adds a brief mention of `Protocol`
    to the description.

--- typing_extensions/setup.py
+++ typing_extensions/setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (2, 7, 0) or (3, 0, 0) <= sys.version_info < (3, 3, 0):
                      'to install the typing package.\n')
     exit(1)
 
-version = '3.6.1'
+version = '3.6.2'
 description = 'Backported and Experimental Type Hints for Python 3.5+'
 long_description = '''\
 Typing Extensions -- Backported and Experimental Type Hints for Python
@@ -22,12 +22,12 @@ able to take advantage of new types added to the ``typing`` module, such as
 
 The ``typing_extensions`` module contains both backports of these changes
 as well as experimental types that will eventually be added to the ``typing``
-module.
+module, such as ``Protocol``.
 
 Users of other Python versions should continue to install and use
-use the ``typing`` module from PyPi instead of using this one unless
-specifically writing code that must be compatible with multiple Python
-versions or requires experimental types.
+the ``typing`` module from PyPi instead of using this one unless specifically
+writing code that must be compatible with multiple Python versions or requires
+experimental types.
 '''
 
 classifiers = [
@@ -53,14 +53,14 @@ else:
 
 install_requires = []
 if sys.version_info < (3, 5):
-    install_requires.append('typing >= 3.6.1')
+    install_requires.append('typing >= 3.6.2')
 
 setup(name='typing_extensions',
       version=version,
       description=description,
       long_description=long_description,
       author='Guido van Rossum, Jukka Lehtosalo, Lukasz Langa, Michael Lee',
-      author_email='jukka.lehtosalo@iki.fi',
+      author_email='levkivskyi@gmail.com',
       url='https://github.com/python/typing',
       license='PSF',
       keywords='typing function annotations type hints hinting checking '
