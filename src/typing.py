a9b24bf724a652b48d0072d60eee09c61e013e17
Ivan Levkivskyi <levkivslyi@gmail.com>
2018-05-10 15:06:42 -0400

    Backport Generic.__new__ fix

--- src/typing.py
+++ src/typing.py
@@ -1182,13 +1182,15 @@ def _generic_new(base_cls, cls, *args, **kwds):
     # Assure type is erased on instantiation,
     # but attempt to store it in __orig_class__
     if cls.__origin__ is None:
-        if base_cls.__new__ is object.__new__:
+        if (base_cls.__new__ is object.__new__ and
+                cls.__init__ is not object.__init__):
             return base_cls.__new__(cls)
         else:
             return base_cls.__new__(cls, *args, **kwds)
     else:
         origin = cls._gorg
-        if base_cls.__new__ is object.__new__:
+        if (base_cls.__new__ is object.__new__ and
+                cls.__init__ is not object.__init__):
             obj = base_cls.__new__(origin)
         else:
             obj = base_cls.__new__(origin, *args, **kwds)
