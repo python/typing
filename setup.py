30b69894e43c930beb6cff6dbb7aafd10954b830
Ivan Levkivskyi <ilevkivskyi@dropbox.com>
2019-08-22 14:19:02 +0100

    Bump version to 3.7.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (2, 7, 0) or (3, 0, 0) <= sys.version_info < (3, 4, 0):
                      'to install the typing package.\n')
     exit(1)
 
-version = '3.7.4'
+version = '3.7.4.1'
 description = 'Type Hints for Python'
 long_description = '''\
 Typing -- Type Hints for Python
