2344c62bd2f8f83a94afbf36e1f6843c18a67c87
dkraczkowski <dawid.kraczkowski@gmail.com>
2021-01-21 10:35:05 +0100

    Bring in protocol’s __init__ behaviour same like in python > 3.8

--- typing_extensions/src_py3/typing_extensions.py
+++ typing_extensions/src_py3/typing_extensions.py
@@ -1119,6 +1119,11 @@ def _is_callable_members_only(cls):
 if hasattr(typing, 'Protocol'):
     Protocol = typing.Protocol
 elif HAVE_PROTOCOLS and not PEP_560:
+
+    def _no_init(self, *args, **kwargs):
+        if type(self)._is_protocol:
+            raise TypeError('Protocols cannot be instantiated')
+
     class _ProtocolMeta(GenericMeta):
         """Internal metaclass for Protocol.
 
@@ -1209,9 +1214,6 @@ elif HAVE_PROTOCOLS and not PEP_560:
                         raise TypeError('Protocols can only inherit from other'
                                         ' protocols, got %r' % base)
 
-                def _no_init(self, *args, **kwargs):
-                    if type(self)._is_protocol:
-                        raise TypeError('Protocols cannot be instantiated')
                 cls.__init__ = _no_init
 
             def _proto_hook(other):
@@ -1365,6 +1367,10 @@ elif HAVE_PROTOCOLS and not PEP_560:
 
 elif PEP_560:
     from typing import _type_check, _GenericAlias, _collect_type_vars  # noqa
+
+    def _no_init(self, *args, **kwargs):
+        if type(self)._is_protocol:
+            raise TypeError('Protocols cannot be instantiated')
 
     class _ProtocolMeta(abc.ABCMeta):
         # This metaclass is a bit unfortunate and exists only because of the lack
@@ -1542,10 +1548,6 @@ elif PEP_560:
                         isinstance(base, _ProtocolMeta) and base._is_protocol):
                     raise TypeError('Protocols can only inherit from other'
                                     ' protocols, got %r' % base)
-
-            def _no_init(self, *args, **kwargs):
-                if type(self)._is_protocol:
-                    raise TypeError('Protocols cannot be instantiated')
             cls.__init__ = _no_init
 
 
