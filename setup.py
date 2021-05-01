05368919636e74232e94a61c7d1ae9fb5d38cc88
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2021-04-30 18:26:53 -0700

    prepare release 3.10.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (2, 7, 0) or (3, 0, 0) <= sys.version_info < (3, 4, 0):
                      'to install the typing package.\n')
     exit(1)
 
-version = '3.7.4.3'
+version = '3.10.0.0'
 description = 'Type Hints for Python'
 long_description = '''\
 Typing -- Type Hints for Python
