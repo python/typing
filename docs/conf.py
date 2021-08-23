82b3940a59d173b210b4d525d0921b25f43669b6
Sebastian Rittau <srittau@rittau.biz>
2021-08-23 19:56:11 +0200

    Ignore venv and README when building docs (#843)
    * Ignore venv and README when building docs
    
    This suppresses warnings during the docs build if the venv directory
    exists.
    
    * Remove html static path from Sphinx config
    
    Fixes another Sphinx warning

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'venv', 'README.rst']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,4 +49,4 @@ html_theme = 'python_docs_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
