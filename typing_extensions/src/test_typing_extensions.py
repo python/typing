9229373d9186aa92d4486c5cf405409a5781e291
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2022-01-15 13:26:08 -0800

    give up on 3.6

--- typing_extensions/src/test_typing_extensions.py
+++ typing_extensions/src/test_typing_extensions.py
@@ -2273,6 +2273,7 @@ class UnpackTests(BaseTestCase):
             Union
         )
 
+    @skipUnless(PEP_560, "Unimplemented for 3.6")
     def test_concatenation(self):
         Xs = TypeVarTuple('Xs')
         Tuple[int, Unpack[Xs]]
@@ -2283,6 +2284,7 @@ class UnpackTests(BaseTestCase):
         C[Unpack[Xs], int]
         C[int, Unpack[Xs], str]
 
+    @skipUnless(PEP_560, "Unimplemented for 3.6")
     def test_class(self):
         Ts = TypeVarTuple('Ts')
 
