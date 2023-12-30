a6b4ad606a1ef66e6e4c04cce672033b9a0075f3
Eric Traut <eric@traut.com>
2023-12-30 00:31:57 -0700

    Added link to conformance results.
    Added tests for `@final` decorator.
    
    Added tests for `Final` annotation.
    
    Updated pyright test results for latest version (1.1.344).
    
    Added tests for Annotated.

---
+++ conformance/tests/qualifiers_annotated.py
@@ -0,0 +1,96 @@
+"""
+Tests the typing.Annotated special form.
+"""
+
+# Specification: https://typing.readthedocs.io/en/latest/spec/qualifiers.html#annotated
+
+# > The first argument to Annotated must be a valid type
+
+from dataclasses import InitVar, dataclass
+from typing import (
+    Annotated,
+    Any,
+    Callable,
+    ClassVar,
+    Final,
+    Literal,
+    NotRequired,
+    Required,
+    TypeAlias,
+    TypeVar,
+    TypedDict,
+    Union,
+)
+
+
+Good1: Annotated[Union[int, str], ""]
+Good2: Annotated[int | None, ""]
+Good3: Annotated[list[int], ""]
+Good4: Annotated[Any, ""]
+Good5: Annotated[tuple[int, ...] | list[int], ""]
+Good6: Annotated[Callable[..., int], ""]
+Good7: Annotated["int | str", ""]
+Good8: Annotated[list["int | str"], ""]
+Good9: Annotated[Literal[3, 4, 5, None], x:=3]
+
+
+async def func3() -> None:
+    Good10: Annotated[str, await func3()]
+
+
+Bad1: Annotated[[int, str], ""]  # Type error: invalid type expression
+Bad2: Annotated[((int, str),), ""]  # Type error: invalid type expression
+Bad3: Annotated[[int for i in range(1)], ""]  # Type error: invalid type expression
+Bad4: Annotated[{"a": "b"}, ""]  # Type error: invalid type expression
+Bad5: Annotated[(lambda: int)(), ""]  # Type error: invalid type expression
+Bad6: Annotated[[int][0], ""]  # Type error: invalid type expression
+Bad7: Annotated[int if 1 < 3 else str, ""]  # Type error: invalid type expression
+Bad8: Annotated[var1, ""]  # Type error: invalid type expression
+Bad9: Annotated[True, ""]  # Type error: invalid type expression
+Bad10: Annotated[1, ""]  # Type error: invalid type expression
+Bad11: Annotated[list or set, ""]  # Type error: invalid type expression
+Bad12: Annotated[f"{'int'}", ""]  # Type error: invalid type expression
+
+
+# > Multiple type annotations are supported (Annotated supports variadic arguments):
+
+Multi1: Annotated[int | str, 3, ""]
+Multi2: Annotated[int | str, 3, "", lambda x: x, max(1, 2)]
+
+# > Annotated must be called with at least two arguments ( Annotated[int] is not valid)
+
+Bad13: Annotated[int]  # Type error: requires at least two arguments
+
+
+# Annotated types can be nested
+
+Nested1: list[Annotated[dict[str, Annotated[str, ""]], ""]]
+Nested2: Annotated[list[Annotated[dict[str, Annotated[Literal[1, 2, 3], ""]], ""]], ""]
+
+
+class ClassA:
+    a: ClassVar[Annotated[int, ""]] = 1
+    b: Annotated[ClassVar[int], ""] = 1
+    c: Final[Annotated[int, ""]] = 1
+    d: Annotated[Final[int], ""] = 1
+
+
+@dataclass
+class ClassB:
+    a: InitVar[Annotated[int, ""]]
+    b: Annotated[InitVar[int], ""]
+
+
+class ClassC(TypedDict):
+    a: Annotated[Required[int], ""]
+    b: Required[Annotated[int, ""]]
+    c: Annotated[NotRequired[int], ""]
+    d: NotRequired[Annotated[int, ""]]
+
+
+TA1: TypeAlias = Annotated[int | str, ""]
+TA2 = Annotated[Literal[1, 2], ""]
+
+T = TypeVar("T")
+
+TA3 = Annotated[T, ""]
---
+++ conformance/tests/qualifiers_final_decorator.py
@@ -0,0 +1,126 @@
+"""
+Tests the @final decorator.
+"""
+
+# Specification: https://typing.readthedocs.io/en/latest/spec/qualifiers.html#final
+
+from typing import final, overload
+from _qualifiers_final_decorator import Base3, Base4
+
+# > A type checker should prohibit any class decorated with @final from being
+# > subclassed and any method decorated with @final from being overridden in a
+# > subclass. The method decorator version may be used with all of instance
+# > methods, class methods, static methods, and properties.
+
+
+@final
+class Base1:
+    ...
+
+
+class Derived1(Base1):  # Type error: Cannot inherit from final class "Base"
+    ...
+
+
+class Base2:
+    @final
+    def method1(self) -> None:
+        pass
+
+    @final
+    @classmethod
+    def method2(cls) -> None:
+        pass
+
+    @final
+    @staticmethod
+    def method3() -> None:
+        pass
+
+    # > For overloaded methods, @final should be placed on the implementation.
+
+    @overload
+    def method4(self, x: int) -> int:
+        ...
+
+    @overload
+    def method4(self, x: str) -> str:
+        ...
+
+    @final
+    def method4(self, x: int | str) -> int | str:
+        return 0
+
+
+class Derived2(Base2):
+    def method1(self) -> None:  # Type error
+        pass
+
+    @classmethod
+    def method2(cls) -> None:  # Type error
+        pass
+
+    @staticmethod
+    def method3() -> None:  # Type error
+        pass
+
+    @overload
+    def method4(self, x: int) -> int:
+        ...
+
+    @overload
+    def method4(self, x: str) -> str:
+        ...
+
+    def method4(self, x: int | str) -> int | str:  # Type error
+        return 0
+
+
+class Derived3(Base3):
+    @overload
+    def method(self, x: int) -> int:
+        ...
+
+    @overload
+    def method(self, x: str) -> str:
+        ...
+
+    def method(self, x: int | str) -> int | str:  # Type error
+        return 0
+
+
+class Derived4(Base4):
+    @overload
+    def method(self, x: int) -> int:
+        ...
+
+    @overload
+    def method(self, x: str) -> str:
+        ...
+
+    def method(self, x: int | str) -> int | str:  # Type error
+        return 0
+
+
+class Base5_1:
+    ...
+
+
+class Base5_2:
+    @final
+    def method(self, v: int) -> None:
+        ...
+
+
+# Test multiple inheritance case.
+class Derived5(Base5_1, Base5_2):
+    def method(self) -> None:  # Type error
+        ...
+
+
+# > It is an error to use @final on a non-method function.
+
+
+@final
+def func1() -> int:  # Type error: not allowed on non-method function.
+    return 0
