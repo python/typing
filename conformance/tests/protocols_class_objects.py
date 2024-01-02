65a4364c41085b0e95d46de0658976adb57a5395
Eric Traut <eric@traut.com>
2024-01-02 01:04:35 -0700

    Added conformance tests for the "Protocols" section of the type specification.

---
+++ conformance/tests/protocols_class_objects.py
@@ -0,0 +1,106 @@
+"""
+Tests the handling of class objects as implementations of a protocol.
+"""
+
+# Specification: https://typing.readthedocs.io/en/latest/spec/protocol.html#type-and-class-objects-vs-protocols
+
+# > Variables and parameters annotated with Type[Proto] accept only concrete
+# (non-protocol) subtypes of Proto.
+
+from abc import abstractmethod
+from typing import Any, ClassVar, Protocol
+
+
+class Proto(Protocol):
+    @abstractmethod
+    def meth(self) -> int:
+        ...
+
+
+class Concrete:
+    def meth(self) -> int:
+        return 42
+
+
+def fun(cls: type[Proto]) -> int:
+    return cls().meth()  # OK
+
+
+fun(Proto)  # Type error
+fun(Concrete)  # OK
+
+
+var: type[Proto]
+var = Proto  # Type error
+var = Concrete  # OK
+var().meth()  # OK
+
+
+# > A class object is considered an implementation of a protocol if accessing
+# > all members on it results in types compatible with the protocol members.
+
+
+class ProtoA1(Protocol):
+    def method1(self, x: int) -> int:
+        ...
+
+
+class ProtoA2(Protocol):
+    def method1(self, obj: Any, x: int) -> int:
+        ...
+
+
+class ConcreteA:
+    def method1(self, x: int) -> int:
+        return 0
+
+
+pa1: ProtoA1 = ConcreteA  # Type error: signatures don't match
+pa2: ProtoA2 = ConcreteA  # OK
+
+
+class ProtoB1(Protocol):
+    @property
+    def prop1(self) -> int:
+        ...
+
+
+class ConcreteB:
+    @property
+    def prop1(self) -> int:
+        return 0
+
+
+pb1: ProtoB1 = ConcreteB  # Type error
+
+
+class ProtoC1(Protocol):
+    attr1: ClassVar[int]
+
+
+class ProtoC2(Protocol):
+    attr1: int
+
+
+class ConcreteC1:
+    attr1: ClassVar[int] = 1
+
+
+class ConcreteC2:
+    attr1: int = 1
+
+
+class CMeta(type):
+    attr1: int
+
+
+class ConcreteC3(metaclass=CMeta):
+    pass
+
+
+pc1: ProtoC1 = ConcreteC1  # Type error
+pc2: ProtoC2 = ConcreteC1  # OK
+pc3: ProtoC1 = ConcreteC2  # Type error
+pc4: ProtoC2 = ConcreteC2  # Type error
+pc5: ProtoC1 = ConcreteC3  # Type error
+pc6: ProtoC2 = ConcreteC3  # OK
