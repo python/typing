33fa565d3290bc33c9b43ff0a5c80f2f45b57d75
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-04-06 07:56:25 -0400

    Conformance test suite: Reduce change of merge conflicts (#1686)
    - Provide option to skip updating timing data
    - Split HTML report into multiple lines
    
    This makes it far less likely that two concurrent PRs to different test cases will have merge conflicts with each other.

--- conformance/src/options.py
+++ conformance/src/options.py
@@ -9,6 +9,7 @@ from dataclasses import dataclass
 @dataclass
 class _Options:
     report_only: bool | None
+    skip_timing: bool
 
 
 def parse_options(argv: list[str]) -> _Options:
@@ -17,7 +18,12 @@ def parse_options(argv: list[str]) -> _Options:
     reporting_group.add_argument(
         "--report-only",
         action="store_true",
-        help=("regenerates the test suite report from past results"),
+        help="regenerates the test suite report from past results",
+    )
+    reporting_group.add_argument(
+        "--skip-timing",
+        action="store_true",
+        help="do not update timing information in the output files",
     )
     ret = _Options(**vars(parser.parse_args(argv)))
     return ret
