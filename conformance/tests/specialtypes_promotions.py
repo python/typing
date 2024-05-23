a442bfe0353973966b39eb8d2232bebeab38a9a7
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-05-23 07:39:15 -0700

    Guard against unreachability

--- conformance/tests/specialtypes_promotions.py
+++ conformance/tests/specialtypes_promotions.py
@@ -13,13 +13,17 @@ v4: complex = 1.2
 v4 = 1
 
 
-def func1(f: float):
+def func1(f: float) -> int:
     f.numerator  # E: attribute exists on int but not float
 
     if isinstance(f, float):
         f.hex()  # OK (attribute exists on float but not int)
+        return 1
     else:
         assert_type(f, int)
+        # Make sure type checkers don't treat this branch as unreachable
+        # and skip checking it.
+        return "x"  # E
 
 
 def func2(x: int) -> float:
