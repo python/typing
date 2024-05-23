6155dbdc1a4553a31a0340c096309f27e7e7ad90
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2024-05-23 07:05:59 -0700

    Extend test

--- conformance/tests/specialtypes_promotions.py
+++ conformance/tests/specialtypes_promotions.py
@@ -6,9 +6,11 @@ from typing import assert_type
 
 # Specification: https://typing.readthedocs.io/en/latest/spec/special-types.html#special-cases-for-float-and-complex
 
-v1: float = 1
-v2: complex = 1.2
-v2 = 1
+v1: int = 1
+v2: float = 1
+v3: float = v1
+v4: complex = 1.2
+v4 = 1
 
 
 def func1(f: float):
