759a5be5192cc91beae672492672c0edc5620b03
Eric Traut <eric@traut.com>
2024-07-27 18:59:02 -0700

    Did a pass on the new deprecated tests to make them stylistically more consistent with existing tests. * Ran code formatter * Moved import statements to top of module * Fixed spelling issues so code passes spell checking * Marked usage of imported deprecated symbol as an optional error

--- conformance/tests/directives_deprecated.py
+++ conformance/tests/directives_deprecated.py
@@ -7,6 +7,9 @@ Tests the warnings.deprecated function.
 # Specification: https://typing.readthedocs.io/en/latest/spec/directives.html#deprecated
 # See also https://peps.python.org/pep-0702/
 
+from typing import Protocol, override
+from typing_extensions import deprecated
+
 # > Type checkers should produce a diagnostic whenever they encounter a usage of an object
 # > marked as deprecated. [...] For deprecated classes and functions, this includes:
 
@@ -14,8 +17,6 @@ Tests the warnings.deprecated function.
 
 from _directives_deprecated_library import Ham  # E: Use of deprecated class Ham
 import _directives_deprecated_library as library
-
-from typing_extensions import deprecated
 
 
 # > * References through module, class, or instance attributes
@@ -30,7 +31,7 @@ library.foo(1)  # E: Use of deprecated overload for foo
 library.foo("x")  # OK
 
 
-ham = Ham()  # OK (already reported above)
+ham = Ham()  # E?: OK (already reported above)
 
 
 # > * Any syntax that indirectly triggers a call to the function.
@@ -48,9 +49,10 @@ spam.shape += "cube"  # E: Use of deprecated property setter Spam.shape
 
 
 class Invocable:
-
     @deprecated("Deprecated")
-    def __call__(self) -> None: ...
+    def __call__(self) -> None:
+        ...
+
 
 invocable = Invocable()
 invocable()  # E: Use of deprecated method __call__
@@ -58,11 +60,13 @@ invocable()  # E: Use of deprecated method __call__
 
 # > * Any usage of deprecated objects in their defining module
 
+
 @deprecated("Deprecated")
-def lorem() -> None: ...
+def lorem() -> None:
+    ...
 
 
-ipsum = lorem()  # E: Use of deprecated function lorem
+lorem()  # E: Use of deprecated function lorem
 
 
 # > There are additional scenarios where deprecations could come into play.
@@ -71,46 +75,46 @@ ipsum = lorem()  # E: Use of deprecated function lorem
 # > As scenarios such as this one appear complex and relatively unlikely to come up in practice,
 # > this PEP does not mandate that type checkers detect them.
 
-from typing import Protocol, override
 
-
-class Fooable(Protocol):
-
+class SupportsFoo1(Protocol):
     @deprecated("Deprecated")
-    def foo(self) -> None: ...
+    def foo(self) -> None:
+        ...
 
-    def bar(self) -> None: ...
+    def bar(self) -> None:
+        ...
 
 
-class Fooer(Fooable):
-
+class FooConcrete1(SupportsFoo1):
     @override
     def foo(self) -> None:  # E?: Implementation of deprecated method foo
         ...
 
-    def bar(self) -> None: ...
+    def bar(self) -> None:
+        ...
 
 
-def foo_it(fooable: Fooable) -> None:
-    fooable.foo()  # E: Use of deprecated method foo
-    fooable.bar()
+def foo_it(f: SupportsFoo1) -> None:
+    f.foo()  # E: Use of deprecated method foo
+    f.bar()
 
 
-# https://github.com/python/typing/pull/1822#discussion_r1693991644
-
-class Fooable2(Protocol):
-
-    def foo(self) -> None: ...
+class SupportsFoo2(Protocol):
+    def foo(self) -> None:
+        ...
 
 
-class Concrete:
-
+class FooConcrete2:
     @deprecated("Deprecated")
-    def foo(self) -> None: ...
+    def foo(self) -> None:
+        ...
 
 
-def take_fooable(f: Fooable2) -> None: ...
+def takes_foo(f: SupportsFoo2) -> None:
+    ...
 
 
-def caller(c: Concrete) -> None:
-    take_fooable(c)  # E?: Concrete is a Fooable2, but only because of a deprecated method
+def caller(c: FooConcrete2) -> None:
+    takes_foo(
+        c
+    )  # E?: FooConcrete2 is a SupportsFoo2, but only because of a deprecated method
