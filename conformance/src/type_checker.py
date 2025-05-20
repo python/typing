ed97c5bea48e217bfa3a19abf46ddb7e8bb0b021
Eric Traut <eric@traut.com>
2025-05-20 09:26:37 -0700

    Removed support for the pytype type checker (#2010)
    Removed support for the pytype type checker.
    
    It is becoming increasingly difficult to maintain support for pytype in the conformance tests. The maintainers of pytype have indicated that they do not have plans to make their type checker compliant with the typing spec, and they've agreed that it makes sense to eliminate it from the conformance test results.

--- conformance/src/type_checker.py
+++ conformance/src/type_checker.py
@@ -7,11 +7,6 @@ import json
 from pathlib import Path
 import os
 import re
-from pytype import config as pytype_config
-from pytype import io as pytype_io
-from pytype import analyze as pytype_analyze
-from pytype.errors import errors as pytype_errors
-from pytype import load_pytd as pytype_loader
 import shutil
 from subprocess import PIPE, CalledProcessError, run
 import sys
@@ -279,114 +274,8 @@ class PyreTypeChecker(TypeChecker):
         return line_to_errors
 
 
-class PytypeTypeChecker(TypeChecker):
-    @property
-    def name(self) -> str:
-        return "pytype"
-
-    def install(self) -> bool:
-        try:
-            # Uninstall any existing version if present.
-            run(
-                [sys.executable, "-m", "pip", "uninstall", "pytype", "-y"],
-                check=True,
-            )
-
-            # Install the latest version.
-            run(
-                [sys.executable, "-m", "pip", "install", "pytype"],
-                check=True,
-            )
-
-            return True
-        except CalledProcessError:
-            print("Unable to install pytype on this platform")
-            return False
-
-    def get_version(self) -> str:
-        proc = run(
-            [sys.executable, "-m", "pytype", "--version"], stdout=PIPE, text=True
-        )
-        version = proc.stdout.strip()
-        return f"pytype {version}"
-
-    def run_tests(self, test_files: Sequence[str]) -> dict[str, str]:
-        # Specify 3.11 for now to work around the fact that pytype
-        # currently doesn't support 3.12 and emits an error when
-        # running on 3.12.
-        options = pytype_config.Options.create(python_version=(3, 11), quick=True)
-        loader = pytype_loader.create_loader(options)
-
-        # Add results to a dictionary keyed by the file name.
-        results_dict: dict[str, str] = {}
-
-        for fi in tqdm(os.listdir(".")):
-            if not fi.endswith(".py"):
-                continue
-            options.tweak(input=fi)
-            with open(fi, "r", encoding="utf-8") as test_file:
-                src = test_file.read()
-            try:
-                analysis: pytype_analyze.Analysis = pytype_io.check_py(
-                    src, options=options, loader=loader
-                )
-            except Exception as e:
-                results_dict[fi] = f"{e.__class__.__name__}: {e}\n"
-            else:
-                results_dict[fi] = self.enforce_consistent_order(
-                    analysis.context.errorlog
-                )
-        return results_dict
-
-    def enforce_consistent_order(self, log: pytype_errors.ErrorLog) -> str:
-        """Pytype does not guarantee deterministic output across runs.
-        It does order diagnostics by line number, but if multiple errors
-        occur on the same line, the ordering appears to change from one
-        run to the next. We require deterministic and consistent output,
-        so this method sorts the pytype output by line number and then
-        alphabetically within a line.
-        """
-
-        class ErrorSorter:
-            def __init__(self, err: pytype_errors.Error) -> None:
-                # Overwrite the details in the error because these can be
-                # nondeterministic (differ from run to run) in some cases.
-                err._details = ""
-                self._err = err
-
-            def __lt__(self, other: "ErrorSorter", /) -> bool:
-                lineno_diff = self._err.line - other._err.line
-                if lineno_diff != 0:
-                    return lineno_diff < 0
-                return other._err.message < self._err.message
-
-            def __eq__(self, other: object, /) -> bool:
-                return (
-                    isinstance(other, ErrorSorter)
-                    and self._err.line == other._err.line
-                    and other._err.message == self._err.message
-                )
-
-        errors: list[pytype_errors.Error] = [
-            error for error in log.unique_sorted_errors()
-        ]
-        errors.sort(key=ErrorSorter)
-        return "\n".join(map(str, errors)) + "\n"
-
-    def parse_errors(self, output: Sequence[str]) -> dict[int, list[str]]:
-        # annotations_forward_refs.py:103:1: unexpected indent [python-compiler-error]
-        line_to_errors: dict[int, list[str]] = {}
-        for line in output:
-            match = re.search(r"^[a-zA-Z0-9_]+.pyi?:(\d+):(\d+): ", line)
-            if match is not None:
-                lineno = int(match.group(1))
-                line_to_errors.setdefault(int(lineno), []).append(line)
-        return line_to_errors
-
-
 TYPE_CHECKERS: Sequence[TypeChecker] = (
     MypyTypeChecker(),
     PyrightTypeChecker(),
     *([] if os.name == "nt" else [PyreTypeChecker()]),
-    PytypeTypeChecker(),
 )
