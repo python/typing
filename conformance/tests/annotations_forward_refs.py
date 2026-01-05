070312df7d477bf14557b1ed7756466b7af494de
Dave Halter <davidhalter88@gmail.com>
2026-01-05 22:38:39 +0100

    Remove error types

--- conformance/tests/annotations_forward_refs.py
+++ conformance/tests/annotations_forward_refs.py
@@ -91,10 +91,6 @@ class ClassD:
         self.ClassC = ClassC()
 
 
-assert_type(ClassD.str, Any)
-assert_type(ClassD.x, Any)
-
-
 # > If a triple quote is used, the string should be parsed as though it is implicitly
 # > surrounded by parentheses. This allows newline characters to be
 # > used within the string literal.
