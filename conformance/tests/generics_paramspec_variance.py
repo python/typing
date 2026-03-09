f7fee1f819d23fd3376f1f400d1fd453b3f46bda
kotlinisland <65446343+kotlinisland@users.noreply.github.com>
2026-03-09 12:58:09 +1000

    specify that parameter specification should have variance

---
+++ conformance/tests/generics_paramspec_variance.py
@@ -0,0 +1,50 @@
+"""
+Tests variance of ParamSpec.
+"""
+
+# Specification: https://typing.readthedocs.io/en/latest/spec/generics.html#semantics
+
+
+from typing import Callable, Generic, ParamSpec
+
+class ContravariantParamSpec[**InP]:
+    def f(self, *args: InP.args, **kwargs: InP.kwargs): ...
+
+in_obj: ContravariantParamSpec[object] = ContravariantParamSpec[int]()  # E
+in_int: ContravariantParamSpec[int] = ContravariantParamSpec[object]()  # OK
+
+
+class CovariantParamSpec[**OutP]:
+    def f(self, fn: Callable[OutP, None]) -> None:
+        raise NotImplementedError
+
+
+out_int: CovariantParamSpec[int] = CovariantParamSpec[object]()  # E
+out_obj: CovariantParamSpec[object] = CovariantParamSpec[int]()  # OK
+
+InP = ParamSpec("InP", contravariant=True)
+
+
+class ContravariantParamSpecOld(Generic[InP]):
+    def in_f(self) -> Callable[InP, None]:  # OK
+        raise NotImplementedError
+
+    def out_f(self, fn: Callable[InP, None]) -> None:  # E
+        raise NotImplementedError
+
+
+in_obj_old: ContravariantParamSpecOld[object] = ContravariantParamSpecOld[int]()  # E
+in_int_old: ContravariantParamSpecOld[int] = ContravariantParamSpecOld[object]()  # OK
+
+OutP = ParamSpec("OutP", covariant=True)
+
+
+class CovariantParamSpecOld(Generic[OutP]):
+    def in_f(self) -> Callable[OutP, None]:  # E
+        raise NotImplementedError
+    def out_f(self, fn: Callable[OutP, None]) -> None:  # OK
+        raise NotImplementedError
+
+
+out_int_old: CovariantParamSpecOld[int] = CovariantParamSpecOld[object]()  # E
+out_obj_old: CovariantParamSpecOld[object] = CovariantParamSpecOld[int]()  # OK
