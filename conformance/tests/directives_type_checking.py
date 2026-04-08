c6fd374da4caa7fc55697038357e311aadadce88
Alex Waygood <alex.waygood@gmail.com>
2026-04-08 18:13:59 +0100

    add an optional `E?` assertion for documentation purposes

--- conformance/tests/directives_type_checking.py
+++ conformance/tests/directives_type_checking.py
@@ -16,3 +16,12 @@ def foo(x: list[int], y: list[str]) -> None:
         z = y
 
     assert_type(z, list[int])
+
+
+if TYPE_CHECKING:
+    x: int = ""  # E: In a `if TYPE_CHECKING` block, type checkers should report all errors as normal
+else:
+    x: int = ""  # E? Many type checkers suppress all errors in `else` blocks of `if TYPE_CHECKING`, though this is not currently specified
+
+if not TYPE_CHECKING:
+    x: int = ""  # E? Many type checkers suppress all errors in `if not TYPE_CHECKING` blocks, though this is not currently specified
